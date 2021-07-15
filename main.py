07560887cd6099b5c557918214e213c422132d53
Valentin Zhizhkun <jijkoun@gmail.com>
2021-07-15 16:54:04 +0200

    test

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """Entry point for this job"""
+import sys
 import pandas
 
 
-def main():
+
+def main(country):
+    print("Country:", country)
     print("Pandas:", pandas)
     print("Test job run successfully")
 
 
 if __name__ == '__main__':
-    main()
+    main(country=sys.argv[1])
